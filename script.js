69bab85a129b57a961e9018bb100e24f6d70f238
HimanshiGoel <himanshigoel248@gmail.com>
2024-04-04 11:25:37 +0530

    added all files

---
+++ script.js
@@ -0,0 +1,10 @@
+const currentTime = () => {
+    let currTime = new Date().toLocaleTimeString() ;
+    document.getElementById('clock').innerText = currTime ;
+}
+
+currentTime();
+
+setInterval(() => {
+    currentTime();
+}, 1000)
